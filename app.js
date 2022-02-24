bf69be57b4bd0ad25574827cac67edcfcd94692f
Nathan Hitchings <nhitchings88@gmail.com>
2022-02-24 14:40:05 -0600

    Improve the wiggl'n and jiggl'n

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var robot = require('robotjs');
+var robot = require("robotjs");
 
-setInterval(function(){ 
-    const Xcoord = Math.floor(Math.random() * 1000) + 1;
-    const Ycoord = Math.floor(Math.random() * 1000) + 1;
-    robot.moveMouse(Xcoord, Ycoord);
-    console.log('wiggln and jiggln');
+console.log("Preparing to wiggle and jiggle...");
+
+const wiggleJiggle = () => {
+  const Xcoord = Math.floor(Math.random() * 1000) + 1;
+  const Ycoord = Math.floor(Math.random() * 1000) + 1;
+  robot.moveMouse(Xcoord, Ycoord);
+  console.log("wiggln and jiggln");
+};
+wiggleJiggle();
+setInterval(function () {
+  wiggleJiggle();
 }, 60000);
